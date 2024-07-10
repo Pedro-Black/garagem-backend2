853e296b31191cc11fdec45d8bc250abd3a56847
Pedro-Black <phrod2007@gmail.com>
2024-07-10 14:47:06 -0300

    criação da api marca

--- app/urls.py
+++ app/urls.py
@@ -9,14 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CorViewSet
-
+from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
-router.register(r"acessorios", AcessorioViewSet)
-router.register(r"categorias", CategoriaViewSet)
-router.register(r"cores", CorViewSet)
+router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
+router.register(r"categorias", CategoriaViewSet,  basename="categorias")
+router.register(r"cores", CorViewSet, basename="cores")
+router.register(r"Marcas", MarcaViewSet, basename="marcas")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
